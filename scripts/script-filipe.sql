e00266bb0fcaa62f1441ac7bc1a043bd80ed3382
Luis Filipe de Lima Nunes <lipinho.nunes2019@gmail.com>
2025-07-04 10:25:29 -0300

    Update script-filipe.sql

--- scripts/script-filipe.sql
+++ scripts/script-filipe.sql
@@ -6,4 +6,12 @@ from
 	produto p
 inner join categoria c on
 	p.categoria_id = c.categoria_id;
---
+--consulta de quantas categorias chamadas "Bebidas" existe
+select
+	count(*) as "Total de Bebidas"
+from
+	produto p
+inner join categoria c on
+	p.categoria_id = c.categoria_id
+where
+	c.categoria = 'Bebidas';

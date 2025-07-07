a90b713021853cf8ba74486f5af25dcfb6e64635
Mathias-hackerman <mathias.souza@aluno.senai.br>
2025-07-07 08:26:01 -0300

    Update script-mathias.sql

--- scripts/script-mathias.sql
+++ scripts/script-mathias.sql
@@ -1,19 +1,25 @@
---Consultando quantos clientes que compraram panos de prato
-select 
-	count(distinct cli.cliente_id) as Clientes_que_compraram_pano_de_prato
-from 
-	compra c
-inner join cliente cli on
-	c.cliente_id = cli.cliente_id
-inner join compra_produto cp on
-	c.compra_id = cp.compra_id
-inner join produto p on
-	p.produto_id = cp.produto_id
-where p.nome = 'Pano de prato';
+--Verificando se o pano está em estoque
+SELECT 
+    COUNT(DISTINCT cli.cliente_id) AS Clientes_que_compraram_pano_de_prato,
+    CASE 
+        WHEN SUM(cp.quantidade) >= p.inventario THEN 'Sem estoque'
+        ELSE 'Em estoque'
+    END AS status_estoque
+FROM 
+    compra c
+INNER JOIN cliente cli ON
+    c.cliente_id = cli.cliente_id
+INNER JOIN compra_produto cp ON
+    c.compra_id = cp.compra_id
+INNER JOIN produto p ON
+    p.produto_id = cp.produto_id
+WHERE 
+    LOWER(p.nome) = 'pano de prato'
+GROUP BY 
+    p.inventario;
 
---Consultando o id do cliente chamado 'Rafael'
-select
-	cli.cliente_id as id_do_rafael
-from
-	cliente cli
-where lower(cli.primeiro_nome) = 'rafael';
+--Consultando as categorias
+SELECT 
+    categoria 
+FROM 
+    categoria;

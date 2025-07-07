617e50373a2d31bb24ca66d4be60e1be1f3fa119
Luis Filipe de Lima Nunes <lipinho.nunes2019@gmail.com>
2025-07-07 08:52:52 -0300

    Update script-filipe.sql

--- scripts/script-filipe.sql
+++ scripts/script-filipe.sql
@@ -1,16 +1,1 @@
---consulta do preço do biscoito
-select
-    p.nome as "Nome do Produto",
-    p.preco as "Preço"
-from
-    produto p
 
---consulta de quantas categorias chamadas "Bebidas" existe
-select
-	count(*) as "Total de Bebidas"
-from
-	produto p
-inner join categoria c on
-	p.categoria_id = c.categoria_id
-where
-	c.categoria = 'Bebidas';
